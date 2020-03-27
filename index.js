2b8074c48a4d26307b99838a192e4a11d23a28f9
nandrease <neeme.andreas.eller@gmail.com>
2020-03-27 12:31:30 +0200

    Interact with login

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const { Builder } = require("selenium-webdriver");
+const { Builder, By } = require("selenium-webdriver");
 
 const driver = new Builder().forBrowser("firefox").build();
 
-async function openWebsites() {
+// async function openWebsites() {
+//   try {
+//     await driver.get("https://www.bigdon.ee");
+//     await driver.get("https://www.addidraws.ee");
+//     await driver.get("https://www.4teams.ee");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// openWebsites();
+
+async function simpleRegistration() {
   try {
-    await driver.get("https://www.bigdon.ee");
-    await driver.get("https://www.addidraws.ee");
-    await driver.get("https://www.4teams.ee");
+    await (await driver).get("https://rori4.github.io/selenium-practice/#/pages/practice/simple-registration");
+    await driver.findElement(By.name("email")).sendKeys("example@gmail.com");
+    await driver.findElement(By.id("password")).sendKeys("asdfg1");
+    await (await driver.findElement(By.name("submit"))).click();
   } catch (error) {
-    console.log(error);
+    console.log("Error: ", error);
   }
 }
 
-openWebsites();
+simpleRegistration();
