e323db597316ed7b1a0fc9b952e6512e418ba88b
nandrease <neeme.andreas.eller@gmail.com>
2020-03-27 12:21:18 +0200

    Async await open websites

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const { Builder, By, Key, util } = require("selenium-webdriver");
+const { Builder } = require("selenium-webdriver");
 
-const firefox = require("selenium-webdriver/firefox");
-const options = new firefox.Options();
+const driver = new Builder().forBrowser("firefox").build();
 
-options.setPreference("browser.download.dir", "C:\\Users\\Einari\\Downloads");
-options.setPreference("browser.download.folderList", 2);
-options.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream", "application/x-csv");
+async function openWebsites() {
+  try {
+    await driver.get("https://www.bigdon.ee");
+    await driver.get("https://www.addidraws.ee");
+    await driver.get("https://www.4teams.ee");
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const driver = new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
-
-driver.get("http://insight.dev.schoolwires.com/HelpAssets/C2Assets/C2Files/C2ImportCalEventSample.csv");
-
-// async function example() {
-//   let driver = await new Builder().forBrowser("firefox").build();
-//   await driver.get("https://www.facebook.com");
-//   await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
-// }
-
-// example();
+openWebsites();
