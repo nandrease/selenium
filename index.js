3a035d01ead865c03220c7b5e778418523fefac7
nandrease <neeme.andreas.eller@gmail.com>
2020-03-27 11:57:43 +0200

    Download CSV with Firefox

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const { Builder, By, Key, util } = require("selenium-webdriver");
 
-async function example() {
-  let driver = await new Builder().forBrowser("chrome").build();
-  await driver.get("http://www.google.com");
-  await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
-}
+const firefox = require("selenium-webdriver/firefox");
+const options = new firefox.Options();
 
-example();
+options.setPreference("browser.download.dir", "C:\\Users\\Einari\\Downloads");
+options.setPreference("browser.download.folderList", 2);
+options.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream", "application/x-csv");
+
+const driver = new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
+
+driver.get("http://insight.dev.schoolwires.com/HelpAssets/C2Assets/C2Files/C2ImportCalEventSample.csv");
+
+// async function example() {
+//   let driver = await new Builder().forBrowser("firefox").build();
+//   await driver.get("https://www.facebook.com");
+//   await driver.findElement(By.name("q")).sendKeys("Selenium", Key.RETURN);
+// }
+
+// example();
